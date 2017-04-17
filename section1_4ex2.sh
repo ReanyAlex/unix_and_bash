90fb8381db70837dadd0fccf25337af1167e87a0
Alex Reany <reanyalex@gmail.com>
2017-04-17 14:39:52 -0700

    Create section1_4ex2.sh

---
+++ section1_4ex2.sh
@@ -0,0 +1,7 @@
+$ echo "From fairest creatures we desire increase,"
+//⌃A
+$ echo "FRom fairest creatures we desire increase,"
+//option click each s delete and replace with ſ
+$ echo "FRom faireſt creatures we deſire increaſe,"
+
+
