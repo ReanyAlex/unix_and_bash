099ef794b0113b8efbf7d72c6bc425bf6933e4ad
Alex Reany <reanyalex@gmail.com>
2017-04-18 12:00:12 -0700

    Delete section2_1ex2.sh

--- command-line/section2_1ex2.sh
+++
@@ -1,5 +0,0 @@
-/created a new text file to hold the first line of the sonnet. Then appended the second line of the sonnet onto the file
-
-echo "From fairest creatures we desire increase," > sonnet_1_copy.txt
-echo "That thereby beauty's rose might never die," >> sonnet_1_copy.txt 
-diff sonnet_1.txt sonnet_1_copy.txt
