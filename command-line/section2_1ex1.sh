d4ee3f2e1351f861926316a63b718f6f8d0dfb39
Alex Reany <reanyalex@gmail.com>
2017-04-17 21:22:51 -0700

    Update section2_1ex1.sh

--- command-line/section2_1ex1.sh
+++ command-line/section2_1ex1.sh
@@ -1,1 +1,8 @@
+//created two new text files from stings using the redirect operator. Ensured files were created correctly with cat
+echo "From fairest creatures we desire increase," > line_1.txt
+echo "That thereby beauty's rose might never die,"  > line_2.txt
+cat line_1.txt
+//From fairest creatures we desire increase,
 
+cat line_2.txt
+//That thereby beauty's rose might never die,
