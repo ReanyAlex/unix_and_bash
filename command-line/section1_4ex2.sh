9d8357458355fcbe8eebe0bdb86782f82389d569
Alex Reany <reanyalex@gmail.com>
2017-04-18 11:59:34 -0700

    Delete section1_4ex2.sh

--- command-line/section1_4ex2.sh
+++
@@ -1,7 +0,0 @@
-$ echo "From fairest creatures we desire increase,"
-//⌃A
-$ echo "FRom fairest creatures we desire increase,"
-//option click each s delete and replace with ſ
-$ echo "FRom faireſt creatures we deſire increaſe,"
-
-
