756e9450c4e4351159886082fa714229233b231c
Alex Reany <reanyalex@gmail.com>
2017-04-20 13:41:09 -0700

    cleaning everything to start on linux

---
+++ command-line/chap3_1ex2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#Using ls, confirm that sonnets.txt exists on your system. How big is it in bytes?
+
+
+ls -l sonnets.txt
---
+++ command-line/chap3_1ex3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#Using the -h (“human-readable”) option to ls, list the
+#long form of the sonnets file with a human-readable byte count.
+
+ls -lh sonnets.txt
